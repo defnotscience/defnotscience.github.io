81a11a68a6a08bf09c875c3dccb5d5f33477b27d
defnotscience <sussyamongus.3043@outlook.com>
2025-12-19 18:14:07 +0000

    r

--- static/assets/scripts/index.js
+++ static/assets/scripts/index.js
@@ -15,7 +15,7 @@ if (form && input) {
 function processUrl(value, path) {
   let url = value.trim()
   const engine = localStorage.getItem('engine')
-  const searchUrl = engine ? engine : 'https://duckduckgo.com/?ia='
+  const searchUrl = engine ? engine : 'https://duckduckgo.com/?q='
   if (!isUrl(url)) {
     url = searchUrl + url
   } else if (!(url.startsWith('https://') || url.startsWith('http://'))) {
